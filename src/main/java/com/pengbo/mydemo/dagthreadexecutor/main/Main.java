32c6b28f1952a8b884f7314e2749f2f9390a748e
pengbo <sunshine_pb@qq.com>
2020-07-06 01:10:48 +0800

    DAGThreadExecutor modify: time、isFork

--- src/main/java/com/pengbo/mydemo/dagthreadexecutor/main/Main.java
+++ src/main/java/com/pengbo/mydemo/dagthreadexecutor/main/Main.java
@@ -9,20 +9,21 @@ import java.util.concurrent.Future;
 public class Main {
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
+        long beginTime = System.currentTimeMillis();
         // build task
-        TreeTask<String> topology1 = new TreeTask("topology1", "topology");
+        TreeTask<String> topology1 = new TreeTask("topology1", "topology", 1000, beginTime);
 
-        TreeTask<String> layer1 = new TreeTask("layer1", "layer");
-        TreeTask<String> layer2 = new TreeTask("layer2", "layer");
+        TreeTask<String> layer1 = new TreeTask("layer1", "layer", 1000, beginTime);
+        TreeTask<String> layer2 = new TreeTask("layer2", "layer", 1000, beginTime);
 
-        TreeTask<String> node1 = new TreeTask("node1", "node");
-        TreeTask<String> node2 = new TreeTask("node2", "node");
-        TreeTask<String> node3 = new TreeTask("node3", "node");
-        TreeTask<String> snode1 = new TreeTask("snode1", "node");
+        TreeTask<String> node1 = new TreeTask("node1", "node", 1000, beginTime);
+        TreeTask<String> node2 = new TreeTask("node2", "node", 1000, beginTime);
+        TreeTask<String> node3 = new TreeTask("node3", "node", 1000, beginTime);
+        TreeTask<String> snode1 = new TreeTask("snode1", "node", 5000, beginTime);
 
-        TreeTask<String> link1 = new TreeTask("link1", "link");
+        TreeTask<String> link1 = new TreeTask("link1", "link", 1000, beginTime);
 
         // build task dependies
         topology1.setChildTasks(Arrays.asList(layer1, layer2));
@@ -39,7 +40,7 @@ public class Main {
         snode1.setParentTasks(Arrays.asList(node1));
         snode1.setChildTasks(Arrays.asList(link1));
 
-        link1.setParentTasks(Arrays.asList(node1, node2));
+        link1.setParentTasks(Arrays.asList(snode1, node2));
 
         layer2.setParentTasks(Arrays.asList(topology1));
         layer2.setChildTasks(Arrays.asList(node3));
@@ -48,8 +49,13 @@ public class Main {
 
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
 
+        System.out.println("begin: " + beginTime);
         Future<String> future = forkJoinPool.submit(topology1);
         String result = future.get();
+
+        //String result=forkJoinPool.invoke(topology1);
+
+        Thread.sleep(20000);
 
         System.out.println(result);
 
--- src/main/java/com/pengbo/mydemo/dagthreadexecutor/model/TreeTask.java
+++ src/main/java/com/pengbo/mydemo/dagthreadexecutor/model/TreeTask.java
@@ -10,34 +10,61 @@ public class TreeTask<T> extends RecursiveTask<T> {
 
     private String type;
 
+    private long sleepTime;
+
+    private long beginTime;
+
+    private int isFork;
+
+
     private List<TreeTask<T>> parentTasks = new ArrayList<>();
 
     private List<TreeTask<T>> childTasks = new ArrayList<>();
 
-    public TreeTask(T name, String type) {
+    public TreeTask(T name, String type, long sleepTime, long beginTime) {
         this.name = name;
         this.type = type;
+        this.sleepTime = sleepTime;
+        this.beginTime = beginTime;
     }
 
     @Override
     protected T compute() {
-
-        // join parentTasks
         String parentResult = "";
-        for (TreeTask<T> task : parentTasks) {
-            parentResult += task.join() + "-";
-        }
-        System.out.println("[" + name + "]" + Thread.currentThread().getName() + " join parentTasks results : " + parentResult);
+        try {
 
-        // process itself
-        parentResult += "-" + name;
-        System.out.println("[" + name + "]" + Thread.currentThread().getName() + " process itself results : " + parentResult);
+            long beginTime = System.currentTimeMillis();
+            System.out.println("[" + name + "]" + Thread.currentThread().getName() + " beginTime : " + (beginTime - this.beginTime));
 
+            // join parentTasks
+            for (TreeTask<T> task : parentTasks) {
+                parentResult += task.join() + "-";
+            }
+            System.out.println("[" + name + "]" + Thread.currentThread().getName() +
+                    " wait... : " + (System.currentTimeMillis() - beginTime) +
+                    " results : " + parentResult);
 
-        // fork childTasks
-        System.out.println("[" + name + "]" + Thread.currentThread().getName() + " fork childTasks");
-        for (TreeTask<T> task : childTasks) {
-            task.fork();
+            // process itself
+            parentResult += "-" + name;
+            System.out.println("[" + name + "]" + Thread.currentThread().getName() + " process itself results : " + parentResult);
+            Thread.sleep(sleepTime);
+
+            // fork childTasks
+            for (TreeTask<T> task : childTasks) {
+                // fork前0
+                // fork后，执行完成前 状态status=0，
+                // 执行完成后 status<0
+                if (task.isFork == 0) {
+                    task.fork();
+                    task.isFork=1;
+                }
+            }
+            System.out.println("[" + name + "]" + Thread.currentThread().getName() + " fork childTasks, " +
+                    "endTime : " + System.currentTimeMillis() +
+                    " total cost : " + (System.currentTimeMillis() - beginTime));
+
+        } catch (Exception e) {
+
         }
 
         return (T) parentResult;
@@ -73,5 +100,30 @@ public class TreeTask<T> extends RecursiveTask<T> {
 
     public void setChildTasks(List<TreeTask<T>> childTasks) {
         this.childTasks = childTasks;
+    }
+
+
+    public long getSleepTime() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(long sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    public long getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(long beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public int getIsFork() {
+        return isFork;
+    }
+
+    public void setIsFork(int isFork) {
+        this.isFork = isFork;
     }
 }
